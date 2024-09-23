dc06ddcf181f723ef2c6903d4042ee58c0464625
hcc <hcc@gmail.com>
2024-09-23 13:17:44 +0200

    borramos los apellidos agregamos compositor favorito

--- Prueba.java
+++ Prueba.java
@@ -1,7 +1,7 @@
 public class Prueba {
 	public static void main (String args[]){
-		System.out.println("Cruz Clavijo");
 		System.out.println("16 años. naci en 2003");
 		System.out.println("Comida fav: pasta");
+		System.out.println("Hans Zimmer Compositor");
 	}	
 }
