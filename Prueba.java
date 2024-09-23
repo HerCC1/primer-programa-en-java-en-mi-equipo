be002e1d1156effef32aeddc08629ac48210ecb5
hcc <hcc@gmail.com>
2024-09-23 12:10:39 +0200

    Agregamos mi comida fav

--- Prueba.java
+++ Prueba.java
@@ -2,5 +2,6 @@ public class Prueba {
 	public static void main (String args[]){
 		System.out.println("Cruz Clavijo");
 		System.out.println("16 años. naci en 2003");
+		System.out.println("Comida fav: pasta");
 	}	
 }
